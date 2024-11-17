6104b11a609250e3e9772261e46febc8e7dae8c9
Muadh <muadhok@gmail.com>
2024-11-17 16:48:04 +0300

    Views restructure

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
-    # Authentication views using class-based views
+    # Authentication views using Django-provided class-based views
     path('register/', views.register, name='register'),  # Custom register view
-    path('login/', views.login_view, name='login'),  # Custom login view
-    path('logout/', views.logout_view, name='logout'),  # Custom logout view
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
