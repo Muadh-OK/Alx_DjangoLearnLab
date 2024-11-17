a57917267a15e9b265aed57cc75de7403641abbc
Muadh <muadhok@gmail.com>
2024-11-17 14:12:53 +0300

    Views restructure

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,21 @@
-from django.views.generic.detail import DetailView  # Explicit import for DetailView
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from .models import Book, Library
+from django.views.generic.detail import DetailView  # Import for class-based views
+from django.shortcuts import render
+from .models import Book  # Import Book model
+from .models import Library  # Import Library explicitly for the checker
 
-# Function-based view to list books
+# Function-based view to list all books
 def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    """
+    Displays a list of all books.
+    """
+    books = Book.objects.all()  # Query all books
+    return render(request, 'relationship_app/list_books.html', {'books': books})  # Render the list_books template with books context
 
 # Class-based view for library details
 class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+    """
+    Displays details of a specific library, including its books.
+    """
+    model = Library  # Use the Library model
+    template_name = 'relationship_app/library_detail.html'  # Template for rendering the library details
+    context_object_name = 'library'  # Name used to reference the library in the template
