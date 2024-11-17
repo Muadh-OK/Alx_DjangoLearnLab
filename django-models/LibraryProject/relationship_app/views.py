8f829b9b4bbcfe63f7a931a3bee40c52e011c148
Muadh <muadhok@gmail.com>
2024-11-17 14:07:03 +0300

    Views restructure

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,7 @@
+from django.views.generic.detail import DetailView  # Explicit import for DetailView
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
-from .models import Library  # Explicit line for Library import
-from .models import Book     # Separate import for Book
+from .models import Book, Library
 
 # Function-based view to list books
 def list_books(request):
